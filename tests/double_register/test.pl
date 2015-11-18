2e3520a205f3cd644d8c714d1e8437446ca03573
kyle <kyle@getaddrinfo.net>
2015-11-17 23:11:30 -0700

    tests: add new double register test
    - try to register the same phone number twice

---
+++ tests/double_register/test.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -I../
+
+use strict;
+use warnings;
+
+use MsgTypes;
+use testlib;
+
+my $sock = new_socket();
+send_msg($sock, $msgs{new_device}, "4038675309");
+my ($type, $response, $length) = recv_msg($sock);
+
+# verify response length is 32 random bytes encoded with base64
+if ($length != 43) {
+	fail "expected response length of 43, got $length";
+}
+
+send_msg($sock, $msgs{new_device}, "4038675309");

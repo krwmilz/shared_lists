873b9d6c8725b82c3a7dbf691374ca646ccbb050
kyle <kyle@getaddrinfo.net>
2015-11-22 22:30:03 -0700

    tests/add_friend: make this test better

--- tests/add_friend/test.pl
+++ tests/add_friend/test.pl
@@ -9,16 +9,12 @@ use test;
 
 my $sock = new_socket();
 send_msg($sock, 'new_device', "4038675309");
-my ($type, $device_id, $length) = recv_msg($sock);
+my (undef, $device_id, undef) = recv_msg($sock);
 
-send_msg($sock, 'add_friend', "$device_id\0" . "4033217654");
-# my ($type2, $list_data, $length2) = recv_msg($sock);
+my $friend_phnum = "4033217654";
+my $send_t = 'add_friend';
+send_msg($sock, $send_t, "$device_id\0$friend_phnum");
+my ($recv_t, $resp_data, $length) = recv_msg($sock);
 
-# fail "got response type $type, expected 1" if ($type2 != 2);
-
-# my ($id, $name, @members) = split("\0", $list_data);
-# my $id_length = length($id);
-# 
-# fail "bad id length $id_length != 43" if ($id_length != 43);
-# fail "recv'd name '$name' not equal to '$list_name'" if ($name ne $list_name);
-# fail "list does not have exactly 1 member" if (@members != 1);
+fail "got response type '$recv_t', expected '$send_t'" if ($recv_t ne $send_t);
+fail "got response ph num '$resp_data' expected '$friend_phnum'" if ($resp_data ne $friend_phnum);

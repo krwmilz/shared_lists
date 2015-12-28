5c0b99af303ae53fda1f15c31626c92f46f07231
kyle <kyle@0x30.net>
2015-12-27 18:13:15 -0700

    tests: fold new_device_bad_phnum into new_device

--- tests/new_device_bad_phnum/test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -I../
-use strict;
-use warnings;
-use test;
-
-# send a new device message with a bad phone number
-my $sock = new_socket();
-send_msg($sock, 'new_device', "403867530&");
-#close($sock);

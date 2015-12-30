193b987688edb112fa91b9b5614c68f13b57c1b1
kyle <kyle@getaddrinfo.net>
2015-12-30 14:02:09 -0700

    tests: fold add_friend_bad_phnum into add_friend

--- tests/add_friend/test.pl
+++ tests/add_friend/test.pl
@@ -8,15 +8,32 @@ use test;
 # - adds a new friend
 
 my $sock = new_socket();
+my $friend1 = "4033217654";
+my $friend2 = "4033217654bad";
+my $msg_good = "friends phone number is not a valid phone number";
 
 send_msg($sock, 'new_device', "4038675309\0unix");
 my ($msg_data) = recv_msg($sock, 'new_device');
 
 my $device_id = check_status($msg_data, 'ok');
-my $friend_phnum = "4033217654";
 
-send_msg($sock, 'add_friend', "$device_id\0$friend_phnum");
+# first verify that a normal add_friend message succeeds
+send_msg($sock, 'add_friend', "$device_id\0$friend1");
 ($msg_data) = recv_msg($sock, 'add_friend');
 
-my $msg = check_status($msg_data, 'ok');
-fail "got response ph num '$msg' expected '$friend_phnum'" if ($msg ne $friend_phnum);
+my $phnum = check_status($msg_data, 'ok');
+fail "got response ph num '$phnum' expected '$friend1'" if ($phnum ne $friend1);
+
+# also verify that a non numeric friends phone number isn't accepted
+send_msg($sock, 'add_friend', "$device_id\0$friend2");
+($msg_data) = recv_msg($sock, 'add_friend');
+
+my $msg = check_status($msg_data, 'err');
+fail "unexpected error message '$msg', expecting '$msg_good'" if ($msg ne $msg_good);
+
+# also verify an empty phone number isn't accepted
+send_msg($sock, 'add_friend', "$device_id\0");
+($msg_data) = recv_msg($sock, 'add_friend');
+
+$msg = check_status($msg_data, 'err');
+fail "unexpected error message '$msg', expecting '$msg_good'" if ($msg ne $msg_good);

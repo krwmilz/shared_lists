96dbe7ba3ee5b44d75773d7fffd640ae8e82455b
kyle <kyle@0x30.net>
2016-01-02 12:41:12 -0700

    gen_msgs: fix up for new paths, and add path for server test suite
    - the server may one day be written in another language, so start separating it
      from its test suite

--- gen_msgs.sh
+++ gen_msgs.sh
@@ -15,8 +15,9 @@ msg_types="
 "
 
 objc_path="ios/shlist/MsgTypes.h"
-perl_path="msgs.pl"
+perl_path="server/msgs.pl"
 java_path="android/shlist/app/src/main/java/drsocto/shlist/MsgTypes.java"
+test_path="server/tests/msgs.pl"
 
 generated_at="generated `date`"
 
@@ -67,6 +68,8 @@ EOF
 	print_table $perl_path "our %msg_num = (" "\$msg => \$i," ");"
 	print_table $perl_path "our @msg_str = (" "'\$msg'," ");"
 	print_table $perl_path "our @msg_func = (" "\\&msg_\$msg," ");"
+
+	cp $perl_path $test_path
 }
 
 gen_objc
--- server/tests/msgs.pl
+++ server/tests/msgs.pl
@@ -1,1 +1,42 @@
-../msgs.pl
+#!/usr/bin/perl
+# generated Mon Dec 28 00:59:49 MST 2015
+use strict;
+use warnings;
+
+our $protocol_ver = 0;
+our %msg_num = (
+	new_device => 0,
+	add_friend => 1,
+	new_list => 2,
+	join_list => 3,
+	leave_list => 4,
+	list_get => 5,
+	list_get_other => 6,
+	list_items => 7,
+	new_list_item => 8,
+	ok => 9,
+);
+our @msg_str = (
+	'new_device',
+	'add_friend',
+	'new_list',
+	'join_list',
+	'leave_list',
+	'list_get',
+	'list_get_other',
+	'list_items',
+	'new_list_item',
+	'ok',
+);
+our @msg_func = (
+	\&msg_new_device,
+	\&msg_add_friend,
+	\&msg_new_list,
+	\&msg_join_list,
+	\&msg_leave_list,
+	\&msg_list_get,
+	\&msg_list_get_other,
+	\&msg_list_items,
+	\&msg_new_list_item,
+	\&msg_ok,
+);

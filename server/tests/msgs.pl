4f1553028ca3c7d5e56923021fad8c12e407f852
kyle <kyle@0x30.net>
2016-01-08 20:54:23 -0700

    gen_msgs: commit newly generated files

--- server/tests/msgs.pl
+++ server/tests/msgs.pl
@@ -1,25 +1,23 @@
 #!/usr/bin/perl
-# generated Sat Jan  2 17:25:28 MST 2016
+# generated Fri Jan  8 20:52:35 MST 2016
 use strict;
 use warnings;
 
 our $protocol_ver = 0;
 our %msg_num = (
 	device_add => 0,
-	device_ok => 1,
-	friend_add => 2,
-	friend_delete => 3,
-	list_add => 4,
-	list_join => 5,
-	list_leave => 6,
-	lists_get => 7,
-	lists_get_other => 8,
-	list_items_get => 9,
-	list_item_add => 10,
+	friend_add => 1,
+	friend_delete => 2,
+	list_add => 3,
+	list_join => 4,
+	list_leave => 5,
+	lists_get => 6,
+	lists_get_other => 7,
+	list_items_get => 8,
+	list_item_add => 9,
 );
 our @msg_str = (
 	'device_add',
-	'device_ok',
 	'friend_add',
 	'friend_delete',
 	'list_add',
@@ -32,7 +30,6 @@ our @msg_str = (
 );
 our @msg_func = (
 	\&msg_device_add,
-	\&msg_device_ok,
 	\&msg_friend_add,
 	\&msg_friend_delete,
 	\&msg_list_add,

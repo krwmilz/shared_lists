3dea64f0aa027c2c354906a8d57b6a624bfea987
kyle <kyle@0x30.net>
2016-01-10 18:49:24 -0700

    tests: spruce up invalid device id test
    - also have this send bad base64 to test that code path in the server

--- server/tests/invalid_deviceid/test.pl
+++ server/tests/invalid_deviceid/test.pl
@@ -7,27 +7,34 @@ use test;
 
 # test that sending invalid device id's results in errors
 
-my $A = client->new();
-$A->set_device_id("");
+my $A = client->new(1);
+my @device_ids = ('', 'somebull$hit');
+my @good_msgs = ('the client sent an unknown device id',
+	'the client sent a device id that was not base64');
 
-# for messages that send 2 arguments, send an empty 2nd argument
-$A->friend_add('', 'err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+for (0..1) {
+	$A->set_device_id($device_ids[$_]);
 
-$A->friend_delete('', 'err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	# for messages that send 2 arguments, send an empty 2nd argument
+	$A->friend_add('', 'err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
 
-$A->list_add('', 'err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	$A->friend_delete('', 'err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
 
-$A->list_join('', 'err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	$A->list_add('', 'err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
 
-$A->list_leave('', 'err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	$A->list_join('', 'err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
 
-$A->lists_get('err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	$A->list_leave('', 'err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
 
-$A->lists_get_other('err');
-fail_msg_ne 'the client sent an unknown device id', $A->get_error();
+	# messages that send 1 argument
+	$A->lists_get('err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
+
+	$A->lists_get_other('err');
+	fail_msg_ne $good_msgs[$_], $A->get_error();
+}

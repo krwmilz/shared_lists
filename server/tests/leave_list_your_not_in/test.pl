84581b16d2f381f2060b99b1121d012969912511
kyle <kyle@0x30.net>
2016-01-12 23:51:18 -0700

    tests: add new test to leave a list your not in
    - makes sure that leaving an otherwise valid list that your not in doesn't work

---
+++ server/tests/leave_list_your_not_in/test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -I../
+use strict;
+use warnings;
+use client;
+use test;
+
+# Send a leave_list message that contains a valid list id but the requesting
+# device is not currently a member of.
+
+my $A = client->new();
+my $B = client->new();
+
+$A->list_add('only a can see this list');
+
+# Who knows how B got this list id, but he did
+$B->list_leave($A->lists(0)->{'id'}, 'err');
+fail_msg_ne 'the client was not a member of the list', $B->get_error();

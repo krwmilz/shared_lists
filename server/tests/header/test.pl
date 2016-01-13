93aa4a2ec5876f42e7e6f5962a93cb30c5fcb537
kyle <kyle@0x30.net>
2016-01-12 23:47:45 -0700

    tests: spruce up comments in header

--- server/tests/header/test.pl
+++ server/tests/header/test.pl
@@ -1,28 +1,26 @@
 #!/usr/bin/perl -I../
 use strict;
 use warnings;
-
 use client;
 
-# Message header sanity checks. A new connection needed every time because
-# server will disconnect on header errors.
+# Need a new connection every time because server disconnects on header errors.
 
-# send an invalid message number
+# Invalid message number
 my $client = client->new(1);
 $client->send_all(pack('nnn', 0, 47837, 0), 6);
 
-# send a bad protocol version
+# Bad protocol version
 $client = client->new(1);
 $client->send_all(pack('nnn', 101, 0, 0), 6);
 
-# send a message length that's too long
+# Payload length that's too long
 $client = client->new(1);
 $client->send_all(pack('nnn', 0, 0, 25143), 6);
 
-# send a message length that's larger than the actual data sent
+# Advertised payload length longer than actual data length
 $client = client->new(1);
 $client->send_all(pack('nnnZ*', 0, 0, 5, 'ab'), 9);
 
-# send a partial header
+# Truncated header
 $client = client->new(1);
 $client->send_all(pack('nn', 101, 69), 4);

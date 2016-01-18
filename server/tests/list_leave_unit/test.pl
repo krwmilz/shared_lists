846f057a7f950e7e2c443e036d0a314f7b960140
kyle <kyle@0x30.net>
2016-01-17 22:45:59 -0700

    tests: capitalize comments in list_leave_unit

--- server/tests/list_leave_unit/test.pl
+++ server/tests/list_leave_unit/test.pl
@@ -1,16 +1,15 @@
 #!/usr/bin/perl -I../
 use strict;
 use warnings;
-
 use client;
 use test;
 
 my $A = client->new();
 
-# try leaving a list your not in
+# Try leaving a list your not in
 $A->list_leave('1234567', 'err');
 fail_msg_ne 'the client sent an unknown list number', $A->get_error();
 
-# try leaving the empty list
+# Try leaving the empty list
 $A->list_leave('', 'err');
 fail_msg_ne 'the client sent a list number that was not a number', $A->get_error();

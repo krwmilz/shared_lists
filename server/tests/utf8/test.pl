b03aa8ab1236b3d95c4e3d400d9869381da4f4e0
kyle <kyle@0x30.net>
2016-01-17 22:41:30 -0700

    sl: enable utf8 support
    - turns out that a nice side effect of sending/receiving json is that it's utf8
      ready by default
      - it wasn't being stored properly in the database though
    - turns out you need to switch on utf8 support in sqlite
      - doing this lets lists with utf8 names be stored and retrieved correctly

---
+++ server/tests/utf8/test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -I../
+use strict;
+use warnings;
+use client;
+use test;
+
+my $A = client->new();
+
+# Create a new list with a name composed of 3 parts:
+# - a left double quotation mark and
+# - ae sorta character thing but where they touch
+# - face with medical mask
+$A->list_add("\xE2\x80\x9C \xC3\xA6 \xF0\x9F\x98\xB8");
+my ($list) = $A->lists_get();
+
+# Check the list name we get back hasn't been mangled in the round trip
+fail_msg_ne "\xE2\x80\x9C \xC3\xA6 \xF0\x9F\x98\xB8", $list->{name};

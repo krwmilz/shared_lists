d4f402c4061888b189d11467f6bb35a86dd8913a
kyle <kyle@getaddrinfo.net>
2016-01-23 14:09:15 -0500

    tests: dial down large_response test for droplet
    - it was taking a long time to run

--- server/tests/large_response/test.pl
+++ server/tests/large_response/test.pl
@@ -8,14 +8,14 @@ use test;
 # only handle that much data at a time
 
 my $A = client->new();
-for (1..500) {
+for (1..200) {
 	$A->list_add($_);
 }
 
-# The response to this lists_get request clocks in at ~59 KB
+# The response to this lists_get request clocks in at ~24 KB
 my $count = 0;
 for my $list ($A->lists_get()) {
 	$count += 1;
 	fail_msg_ne "$count", $list->{name};
 }
-fail_num_ne 'total lists different', $count, 500;
+fail_num_ne 'total lists different', $count, 200;

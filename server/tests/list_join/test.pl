f0fb01fd9c9206be5d014f735796be53d1a96aff
Kyle Milz <kyle@0x30.net>
2016-01-24 20:06:19 -0700

    sl: send back more information in list_join response

--- server/tests/list_join/test.pl
+++ server/tests/list_join/test.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -I../
 use strict;
 use warnings;
-
 use client;
 use test;
 
@@ -15,12 +14,20 @@ $B->friend_add($A->phnum());
 # A creates a new list
 my $list_name = "this is a new list";
 $A->list_add($list_name);
+my $list_num = $A->lists(0)->{num};
 
 # B joins A's list
-$B->list_join($A->lists(0)->{num});
+my $response = $B->list_join($list_num);
+my $list = $response->{list};
+fail_num_ne 'list num mismatch', $list->{num}, $list_num;
+fail_msg_ne 'this is a new list', $list->{name};
+fail_num_ne 'date mismatch', $list->{date}, 0;
+fail_num_ne 'items complete mismatch', $list->{items_complete}, 0;
+fail_num_ne 'items total mismatch', $list->{items_total}, 0;
+fail_num_ne 'num members mismatch', $list->{num_members}, 2;
 
 # B requests its lists to make sure its committed to the list
-my ($list) = $B->lists_get();
+($list) = $B->lists_get();
 
 # Verify what we get from server
 my $stored_list = $A->lists(0);

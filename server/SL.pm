f60cfdb5914487b55741a5cb4184da36e0c81c38
Kyle Milz <kyle@0x30.net>
2016-02-27 12:29:40 -0700

    server: fold testd.pl into SL.pm
    - we'll throw a class wrapper around it for easy use

--- server/SL.pm
+++ server/SL.pm
@@ -258,3 +258,54 @@ sub msg_str {
 }
 
 1;
+
+package SL::Notify;
+use strict;
+
+use IO::Socket::UNIX;
+use JSON::XS;
+
+sub new {
+	my $class = shift;
+
+	my $self = {};
+	bless ($self, $class);
+
+	$self->{socket_path} = "../testd.socket";
+
+	my $server = IO::Socket::UNIX->new(
+		Type => SOCK_STREAM(),
+		Local => $self->{socket_path},
+		Listen => 1,
+	);
+	die "$self->{socket_path}: couldn't create socket: $!\n" unless ($server);
+
+	while (my $client = $server->accept()) {
+		$client->read(my $data, 4096);
+		my $notify = decode_json($data);
+
+		my $num_devices = @{ $notify->{devices} };
+		next if ($num_devices == 0);
+
+		print "testd: message type '$notify->{msg_type}'\n";
+		# print "testd: payload is '" . Dumper($notify->{payload}) . "'\n";
+
+		for (@{ $notify->{devices} }) {
+			#print Dumper($_);
+			my ($os, $push_token) = @$_;
+			print "testd: sending to '$push_token' os '$os'\n";
+		}
+	}
+
+	return $self;
+}
+
+sub DESTROY {
+	my $self = shift;
+
+	unlink $self->{socket_path};
+	#kill 'TERM', $self->{pid};
+	#waitpid( $self->{pid}, 0 );
+}
+
+1;
